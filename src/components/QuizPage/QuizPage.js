0f3210613d24dd1b9f1af7a75377fa1f51d37cb3
tamim-i-chowdhury <tamim.c183@gmail.com>
2022-10-12 05:54:02 +0600

    toast added for eyes

--- src/components/QuizPage/QuizPage.js
+++ src/components/QuizPage/QuizPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import QuizQuestion from "../QuizQuestion/QuizQuestion";
 
@@ -6,14 +6,27 @@ const QuizPage = () => {
   const dataLoader = useLoaderData();
   const quiz = dataLoader.data;
   const { name, total, questions } = quiz;
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const handleCorrectAnswer = (ca) => {
+    setCorrectAnswer(ca);
+  };
   return (
     <div className="mt-16">
-      <h1 className="text-center text-3xl font-medium">Quiz on {name}</h1>
-      <div>
-        {questions.map((quizQuestion) => (
+      <div className="toast toast-middle toast-end">
+        <div className="alert alert-info">
+          <div>
+            <span>{correctAnswer}</span>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-center text-4xl font-medium">Quiz on {name}</h1>
+      <div className="flex flex-col items-center">
+        {questions.map((quizQuestion, index) => (
           <QuizQuestion
             key={quizQuestion.id}
             quizQuestion={quizQuestion}
+            index={index}
+            handleCorrectAnswer={handleCorrectAnswer}
           ></QuizQuestion>
         ))}
       </div>
--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -1,20 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+
 import React from "react";
 
-const QuizQuestion = ({ quizQuestion }) => {
+const QuizQuestion = ({ quizQuestion, index, handleCorrectAnswer }) => {
   const { question, options, correctAnswer } = quizQuestion;
   return (
-    <div>
-      <div className="card card-side bg-base-100 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/200/280/arch" alt="Movie" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
+    <div className="card w-11/12 bg-white shadow-xl my-12 lg:w-1/2">
+      <div className="flex justify-between pr-6 pl-10 my-10">
+        <h2 className="card-title mr-8">
+          Quiz {index + 1}: {question}
+        </h2>
+        <button onClick={() => handleCorrectAnswer(correctAnswer)}>
+          <FontAwesomeIcon
+            className="mt-2 text-2xl"
+            icon={faEye}
+          ></FontAwesomeIcon>
+        </button>
+      </div>
+      <div className="flex flex-col items-center">
+        {options.map((option) => (
+          <div className="form-control w-5/6 bg-gray-200 my-2 px-4 py-2 rounded-lg lg:w-4/6">
+            <label className="label cursor-pointer flex justify-start">
+              <input
+                type="radio"
+                name="radio-6"
+                className="radio checked:bg-blue-500 mr-5"
+              />
+              <span className="text-normal">{option}</span>
+            </label>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
