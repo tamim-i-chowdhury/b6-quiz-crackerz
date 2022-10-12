448320d3352808a09570c29cf90dcb61211b2a6b
tamim-i-chowdhury <tamim.c183@gmail.com>
2022-10-12 23:45:58 +0600

    fully mobile responsive now

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <div>
       <div className="hero bg-blue-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={logo} className="max-w-sm rounded-lg shadow-2xl" />
+          <img src={logo} className="max-w-xs rounded-lg shadow-2xl" />
           <div>
             <h1 className="text-3xl text-center mt-4 font-bold lg:text-5xl lg:text-start">
               The Purpose of Quiz!
