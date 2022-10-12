82a211c5b3f9a6b286f86665e6985e98014dcd35
tamim-i-chowdhury <tamim.c183@gmail.com>
2022-10-12 22:58:02 +0600

    toast modified for correct answer

--- src/components/QuizPage/QuizPage.js
+++ src/components/QuizPage/QuizPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import QuizQuestion from "../QuizQuestion/QuizQuestion";
 
 const QuizPage = () => {
@@ -9,16 +11,31 @@ const QuizPage = () => {
   const [correctAnswer, setCorrectAnswer] = useState("");
   const handleCorrectAnswer = (ca) => {
     setCorrectAnswer(ca);
+    toast(ca, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
   };
   return (
     <div className="mt-16">
-      <div className="toast toast-middle toast-end">
-        <div className="alert alert-info">
-          <div>
-            <span>{correctAnswer}</span>
-          </div>
-        </div>
-      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <h1 className="text-center text-4xl font-medium">Quiz on {name}</h1>
       <div className="flex flex-col items-center">
         {questions.map((quizQuestion, index) => (
