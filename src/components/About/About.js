c6cd1da14a8110ae3e31d5af76073401fac004f7
tamim-i-chowdhury <tamim.c183@gmail.com>
2022-10-12 23:23:27 +0600

    everything done now

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h3>You can found us through this page</h3>
+    <div className="bg-blue-200 w-full h-full">
+      <h1 className="text-center text-3xl pt-60 pb-96 lg:text-5xl">
+        Content will be updated in future.
+      </h1>
     </div>
   );
 };
--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -8,13 +8,31 @@ const QuizQuestion = ({ quizQuestion, index, handleCorrectAnswer }) => {
   const { question, options, correctAnswer } = quizQuestion;
   const handleAnswerChecker = (option) => {
     if (option === correctAnswer) {
-      toast("Your answer is correct.");
+      toast("Your answer is correct.", {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     } else {
-      toast("Your answer is wrong.");
+      toast("Your answer is wrong.", {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
   };
   return (
-    <div className="card w-11/12 bg-white shadow-xl my-12 lg:w-1/2">
+    <div className="card w-11/12 bg-gray-200 shadow-xl my-12 lg:w-1/2">
       <div className="flex justify-between pr-6 pl-10 my-10">
         <h2 className="card-title mr-8">
           Quiz {index + 1}: {question}
@@ -26,7 +44,7 @@ const QuizQuestion = ({ quizQuestion, index, handleCorrectAnswer }) => {
           ></FontAwesomeIcon>
         </button>
       </div>
-      <div className="flex flex-col items-center bg-red-400">
+      <div className="flex flex-col items-center bg-gray-800">
         {options.map((option) => (
           <div className="form-control w-5/6 bg-gray-200 my-2 px-4 py-2 rounded-lg lg:w-4/6">
             <label className="label cursor-pointer flex justify-start">
@@ -34,13 +52,24 @@ const QuizQuestion = ({ quizQuestion, index, handleCorrectAnswer }) => {
                 onClick={() => handleAnswerChecker(option)}
                 type="radio"
                 name="radio-6"
-                className="radio checked:bg-blue-500 mr-5"
+                className="radio checked:bg-blue-700 mr-5"
               />
               <span className="text-normal">{option}</span>
             </label>
           </div>
         ))}
-        <ToastContainer position="top-left"></ToastContainer>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </div>
     </div>
   );
